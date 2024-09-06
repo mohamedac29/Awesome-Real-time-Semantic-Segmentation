a3fd6874983ef4c8ff1a94cbd809c2ce23743bec
mohamedac29 <73788860+mohamedac29@users.noreply.github.com>
2024-09-06 23:11:39 +0800

    Update model_builder.py

--- model_builder/model_builder.py
+++ model_builder/model_builder.py
@@ -2,12 +2,15 @@ from models.DSANet import DSANet
 from models.SPFNet import SPFNet
 from models.SSFPN import SSFPN
 from models.DABNet import DABNet
+from models.CGNet import CGNet
 
 def build_model(model_name,num_classes):
     if model_name == 'DABNet':
         return DABNet(classes=num_classes)
     elif model_name == 'DSANet':
         return DSANet(classes=num_classes)
+    elif model_name == 'CGNet':
+        return CGNet(classes=num_classes)
     elif model_name == 'SPFNet':
         return SPFNet("resnet18",pretrained=True,classes=num_classes)
     elif model_name == 'SSFPN':
